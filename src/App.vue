701ea54c6c547e1ee819e37a6979fd4468d97964
hasnaa-ux <hasnaa3193378@gmail.com>
2022-11-12 01:56:13 +0200

    c2

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,17 @@
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
-
-  <RouterView />
+  <main>
+    <p>hello</p>
+  </main>
 </template>
 
-<script >
-import { RouterLink, RouterView } from "vue-router";
+<script>
 export default {
-  components: {   
-  },
+  components: {},
   data() {
     return {
-      api_key:'893e4e2c9ea095ec2eeb47515bcfb300',
+      api_key: "893e4e2c9ea095ec2eeb47515bcfb300",
     };
   },
-  
 };
 </script>
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
